fed8d14fa92a599eeecb5b7b80d2a941f9223976
angelalita77 <anjeanette@gmail.com>
2025-09-07 01:57:40 -0400

    successfully completed POST method of a new user. Completed test methods for GET, PATCH, DELETE by id for /api/users/:id

--- routes/userRoutes.mjs
+++ routes/userRoutes.mjs
@@ -2,28 +2,57 @@ import express from "express";
 import { users } from "../data/users.mjs";
 const router = express.Router();
 
-// @route GET /api/users
-// @desc Get all users
-// @access Public
+//GET and POST /api/users
+router
+    .route("/")
+    // Create
+    // @route POST /api/users
+    // @desc create all users
+    // @access Public
+    .post((req, res) => {
+        const { name, username, email } = req.body;
 
-// Create
-router.post("/", (req, res) => {
-    res.json( {msg: `Testing create`});
+        // Check if we have all data needed to create user
+        if (name && username && email) {
+            // check is username exists!!
+            if (users.find((u) => u.username == username)) {
+                res.status(400).json({ err: "Username taken" });
+                return;
+            }
+
+            const user = {
+                id: users[users.length - 1].id + 1, //find the last users id number and add one to it.
+                name,
+                username,
+                email,
+            };
+            users.push(user);
+            res.json(user);
+        } else res.status(400).json({ msg: "Insuffecient Data" });
+    })
+    // Read
+    // @route GET /api/users
+    // @desc get all users
+    // @access Public
+    .get((req, res) => {
+        res.json(users);
+    })
+
+    // GET, POST, PATCH, DELETE /api/users/:id
+router
+    .route("/:id")
+    //  @route GET /api/users/:id
+//  @desc Get ONE user
+//  @access Public
+.get( (req, res) => {
+    res.json( {msg: 'Testing read by id'})
 })
-
-// Read
-router.get("/", (req, res) => {
-    res.json( {msg: `Testing read`});
-})
-
-
-// Update
-router.put("/:id", (req, res) => {
+.patch( (req, res) => {
     res.json( {msg: `Testing update`});
 })
 
 // Delete
-router.delete("/:id", (req, res) => {
+.delete( (req, res) => {
     res.json( {msg: `Testing delete`});
 })
 
