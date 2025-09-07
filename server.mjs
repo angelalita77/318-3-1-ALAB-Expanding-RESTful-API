749713a4828834bab1867dbf3769d218760572f2
angelalita77 <anjeanette@gmail.com>
2025-09-06 21:06:59 -0400

    added users and post API contentin data folder

---
+++ server.mjs
@@ -0,0 +1,33 @@
+//Imports 
+import express, { Router } from "express";
+import globalErr from "./globalErr_handling/globalErrHandling'.mjs"
+
+
+// Env Setup
+const app = express();
+const PORT = 3000;
+
+
+// Middleware
+
+
+
+// Routes
+
+
+
+
+// Global Err Handling
+app.use(globalErr);
+
+// Test global error throw
+// app.get("/error-test", (req, res, next) => {
+//   const err = new Error("Something went wrong!");
+//   next(err); // sends error to your global handler
+// });
+
+// Server Listening
+app.listen(PORT, () => {
+    console.log(`Server Running on PORT: ${PORT}`);
+});
+
